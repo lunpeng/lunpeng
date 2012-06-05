b03a5303f43daf3f7558dc35d86f434c12143601
Lun Peng <lunpeng.uncc@gmail.com>
2012-06-05 14:39:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Temeertest::Application.config.secret_token = '68d162d0f3098d9abf1765343fb72e377eefe329fea66eed046fbe867534281b200b68e53cc9370334595d9ffe0f153acd037157a1f0eb07105bdd06ed10be6a'
